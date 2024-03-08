0f7d4487d923123bcd5068c8ed2798b72b473b08
Ype Kingma <ykingma@freedom.nl>
2024-03-08 15:56:11 +0100

    Add first test code for function argument

--- tests/ui/lifetimes_bound_double_ref.rs
+++ tests/ui/lifetimes_bound_double_ref.rs
@@ -1,5 +1,15 @@
 #![warn(clippy::lifetimes_bound_double_ref)]
 
+// missing bound
+pub fn lifetime_translator_1<'a, 'b, T>(val_a: &'a &'b (), _val_b: &'b T) -> &'a T {
+    val_a
+}
+
+// with bound
+pub fn lifetime_translator_2<'a, 'b: 'a, T>(val_a: &'a &'b (), _val_b: &'b T) -> &'a T {
+    val_a
+}
+
 fn main() {
     // test code goes here
 }
