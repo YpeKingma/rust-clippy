7dadd7520d4b7d2ce04b7dee11e775106c4059bf
Ype Kingma <ykingma@freedom.nl>
2024-03-08 16:16:51 +0100

    Correct the lint name

--- tests/ui/lifetimes_bound_double_ref.rs
+++ tests/ui/lifetimes_bound_double_ref.rs
@@ -1,4 +1,4 @@
-#![warn(clippy::lifetimes_bound_double_ref)]
+#![warn(clippy::add_redundant_lifetimes_bound_double_ref_arg)]
 
 // missing bound
 pub fn lifetime_translator_1<'a, 'b, T>(val_a: &'a &'b T, _val_b: &'b T) -> &'a T {
