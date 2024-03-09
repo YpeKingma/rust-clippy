51f4b62a5df7c5e30909ab589fe61ec1dd1f3061
Ype Kingma <ykingma@freedom.nl>
2024-03-09 13:01:13 +0100

    Move the lifetimes bound to front

--- tests/ui/lifetimes_bound_double_ref.rs
+++ tests/ui/lifetimes_bound_double_ref.rs
@@ -1,12 +1,12 @@
 #![warn(clippy::add_redundant_lifetimes_bound_double_ref_arg)]
 
 // missing bound
-pub fn lifetime_translator_1<'a, 'b, T>(val_a: &'a &'b T, _val_b: &'b T) -> &'a T {
+pub fn lifetime_translator_1<'a, 'b: 'a, T>(val_a: &'a &'b T, _val_b: &'b T) -> &'a T {
     val_a
 }
 
 // with bound
-pub fn lifetime_translator_2<'a, 'b: 'a, T>(val_a: &'a &'b T, _val_b: &'b T) -> &'a T {
+pub fn lifetime_translator_2<'a, 'b, T>(val_a: &'a &'b T, _val_b: &'b T) -> &'a T {
     val_a
 }
 
