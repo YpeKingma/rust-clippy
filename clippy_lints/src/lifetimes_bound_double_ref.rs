e5d6e4b4e822397914b12b429869e41ccd701974
Ype Kingma <ykingma@freedom.nl>
2024-03-10 12:37:54 +0100

    Reduce test output to lifetime bounds and lifetimes of double ref inputs.

--- clippy_lints/src/lifetimes_bound_double_ref.rs
+++ clippy_lints/src/lifetimes_bound_double_ref.rs
@@ -1,4 +1,5 @@
 use rustc_hir::intravisit::FnKind;
+use rustc_hir::{GenericBound, TyKind, WherePredicate};
 use rustc_lint::{LateContext, LateLintPass};
 use rustc_session::impl_lint_pass;
 
@@ -46,18 +47,62 @@ impl LateLintPass<'_> for LifetimesBoundDoubleRef {
         fn_decl: &'tcx rustc_hir::FnDecl<'tcx>,
         _body: &'tcx rustc_hir::Body<'tcx>,
         _span: rustc_span::Span,
-        local_def_id: rustc_span::def_id::LocalDefId,
+        _local_def_id: rustc_span::def_id::LocalDefId,
     ) {
-        dbg!(local_def_id);
-        dbg!(fn_kind); // includes generics
         let FnKind::ItemFn(ident, generics, _fn_header) = fn_kind else {
-            return; // 
+            return;
         };
-        dbg!(ident);
-        // collect predicate bounds on lifetime pairs
-        for predicate in generics.predicates {
-            // etc.
+        if generics.predicates.is_empty() && fn_decl.inputs.is_empty() {
+            return;
         }
-        dbg!(fn_decl); // function argument types as inputs
+        dbg!("ItemFn", ident);
+        // collect declared predicate bounds on lifetime pairs
+        for predicate in generics.predicates {
+            match predicate {
+                WherePredicate::BoundPredicate(where_bound_predicate) => {
+                    dbg!("ignoring where_bound_predicate", where_bound_predicate);
+                    continue;
+                },
+                WherePredicate::RegionPredicate(where_region_predicate) => {
+                    if where_region_predicate.in_where_clause {
+                        dbg!(
+                            "ignoring where_region_predicate in_where_clause",
+                            where_region_predicate
+                        );
+                        continue;
+                    }
+                    let lifetime = where_region_predicate.lifetime;
+                    dbg!(lifetime);
+                    for generic_bound in where_region_predicate.bounds {
+                        let GenericBound::Outlives(outlived_lifetime) = generic_bound else {
+                            continue;
+                        };
+                        dbg!(outlived_lifetime);
+                    }
+                },
+                WherePredicate::EqPredicate(eq_predicate) => {
+                    dbg!("ignoring eq_predicate", eq_predicate);
+                    continue;
+                },
+            }
+        }
+
+        // collect double references with lifetimes from input arguments
+        dbg!("fn_decl.inputs");
+        for input_ty in fn_decl.inputs {
+            let TyKind::Ref(mut lifetime, mut mut_ty) = input_ty.kind else {
+                dbg!("no lifetime for:");
+                dbg!(input_ty.kind);
+                continue;
+            };
+            while let TyKind::Ref(next_lifetime, next_mut_ty) = mut_ty.ty.kind {
+                dbg!("lifetime pair from function input types:");
+                dbg!(lifetime);
+                dbg!(next_lifetime);
+
+                mut_ty = next_mut_ty;
+                lifetime = next_lifetime;
+            }
+        }
     }
 }
