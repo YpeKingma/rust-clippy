540f044c35c2cf3532d04cc90866d69a0ee87d29
Ype Kingma <ykingma@freedom.nl>
2024-03-10 13:42:27 +0100

    Use LifetimeName to collect lifetime bounds

--- clippy_lints/src/lifetimes_bound_double_ref.rs
+++ clippy_lints/src/lifetimes_bound_double_ref.rs
@@ -1,5 +1,5 @@
 use rustc_hir::intravisit::FnKind;
-use rustc_hir::{GenericBound, TyKind, WherePredicate};
+use rustc_hir::{GenericBound, LifetimeName, TyKind, WherePredicate};
 use rustc_lint::{LateContext, LateLintPass};
 use rustc_session::impl_lint_pass;
 
@@ -49,60 +49,60 @@ impl LateLintPass<'_> for LifetimesBoundDoubleRef {
         _span: rustc_span::Span,
         _local_def_id: rustc_span::def_id::LocalDefId,
     ) {
-        let FnKind::ItemFn(ident, generics, _fn_header) = fn_kind else {
+        let FnKind::ItemFn(_ident, generics, _fn_header) = fn_kind else {
             return;
         };
         if generics.predicates.is_empty() && fn_decl.inputs.is_empty() {
             return;
         }
-        dbg!("ItemFn", ident);
+        #[derive(Debug)]
+        struct BoundLifetimePair {
+            long_lifetime_name: LifetimeName,
+            outlived_lifetime_name: LifetimeName,
+        }
+        let mut declared_bounds = Vec::<BoundLifetimePair>::new();
+        let mut implied_bounds = Vec::<BoundLifetimePair>::new();
         // collect declared predicate bounds on lifetime pairs
         for predicate in generics.predicates {
             match predicate {
-                WherePredicate::BoundPredicate(where_bound_predicate) => {
-                    dbg!("ignoring where_bound_predicate", where_bound_predicate);
+                WherePredicate::BoundPredicate(_) | WherePredicate::EqPredicate(_) => {
                     continue;
                 },
                 WherePredicate::RegionPredicate(where_region_predicate) => {
-                    if where_region_predicate.in_where_clause {
-                        dbg!(
-                            "ignoring where_region_predicate in_where_clause",
-                            where_region_predicate
-                        );
-                        continue;
-                    }
-                    let lifetime = where_region_predicate.lifetime;
-                    dbg!(lifetime);
+                    // ignore where_region_predicate.in_where_clause, both cases ok.
                     for generic_bound in where_region_predicate.bounds {
                         let GenericBound::Outlives(outlived_lifetime) = generic_bound else {
                             continue;
                         };
-                        dbg!(outlived_lifetime);
+                        let declared_bound_lifetime_pair = BoundLifetimePair {
+                            long_lifetime_name: where_region_predicate.lifetime.res,
+                            outlived_lifetime_name: outlived_lifetime.res,
+                        };
+                        declared_bounds.push(declared_bound_lifetime_pair);
                     }
-                },
-                WherePredicate::EqPredicate(eq_predicate) => {
-                    dbg!("ignoring eq_predicate", eq_predicate);
-                    continue;
                 },
             }
         }
 
         // collect double references with lifetimes from input arguments
-        dbg!("fn_decl.inputs");
         for input_ty in fn_decl.inputs {
             let TyKind::Ref(mut lifetime, mut mut_ty) = input_ty.kind else {
                 dbg!("no lifetime for:");
                 dbg!(input_ty.kind);
                 continue;
             };
-            while let TyKind::Ref(next_lifetime, next_mut_ty) = mut_ty.ty.kind {
-                dbg!("lifetime pair from function input types:");
-                dbg!(lifetime);
-                dbg!(next_lifetime);
+            while let TyKind::Ref(nested_lifetime, next_mut_ty) = mut_ty.ty.kind {
+                let implied_bound_lifetime_pair = BoundLifetimePair {
+                    long_lifetime_name: nested_lifetime.res,
+                    outlived_lifetime_name: lifetime.res,
+                };
+                implied_bounds.push(implied_bound_lifetime_pair);
 
                 mut_ty = next_mut_ty;
-                lifetime = next_lifetime;
+                lifetime = nested_lifetime;
             }
         }
+        dbg!(&declared_bounds);
+        dbg!(&implied_bounds);
     }
 }
