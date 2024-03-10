56b35f8b07220c3eb1f4a297350547125139111a
Ype Kingma <ykingma@freedom.nl>
2024-03-10 15:43:56 +0100

    Correct test code comments

--- tests/ui/lifetimes_bound_double_ref.rs
+++ tests/ui/lifetimes_bound_double_ref.rs
@@ -1,11 +1,11 @@
 #![warn(clippy::add_redundant_lifetimes_bound_double_ref_arg)]
 
-// missing bound
+// with bound
 pub fn lifetime_translator_1<'lfta, 'lftb: 'lfta, T>(val_a: &'lfta &'lftb T, _val_b: &'lftb T) -> &'lfta T {
     val_a
 }
 
-// with bound
+// missing bound
 pub fn lifetime_translator_2<'lfta, 'lftb, T>(val_a: &'lfta &'lftb T, _val_b: &'lftb T) -> &'lfta T {
     val_a
 }
