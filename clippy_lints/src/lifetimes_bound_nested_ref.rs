34fa42fb7ecd87b834bc183d0b56ea7f845cc2e8
Ype Kingma <ykingma@freedom.nl>
2024-03-25 10:56:31 +0100

    Use lifetimes from ADTs, fix a few bugs and a message typo, add test cases.

--- clippy_lints/src/lifetimes_bound_nested_ref.rs
+++ clippy_lints/src/lifetimes_bound_nested_ref.rs
@@ -25,20 +25,21 @@ use std::cmp::Ordering;
 use std::collections::BTreeSet;
 
 use clippy_utils::diagnostics::span_lint;
+use rustc_hir::def_id::LocalDefId;
 use rustc_hir::intravisit::FnKind;
-use rustc_hir::{
-    GenericArg, GenericBound, GenericParam, GenericParamKind, Generics, Item, ItemKind, LifetimeParamKind,
-    WherePredicate,
-};
+use rustc_hir::{Body, FnDecl, GenericArg, GenericBound, Generics, Item, ItemKind, ParamName, WherePredicate};
 use rustc_hir_analysis::hir_ty_to_ty;
 use rustc_lint::{LateContext, LateLintPass};
 use rustc_middle::ty::ty_kind::TyKind;
-use rustc_middle::ty::{BoundRegionKind, RegionKind, Ty};
+use rustc_middle::ty::{Region, Ty};
 use rustc_session::impl_lint_pass;
-use rustc_span::Symbol;
+use rustc_span::{Span, Symbol};
 
 extern crate rustc_type_ir;
 use rustc_type_ir::AliasKind;
+
+extern crate rustc_hash;
+use rustc_hash::FxHashSet;
 
 declare_clippy_lint! {
     /// ### What it does
@@ -116,32 +117,30 @@ impl<'tcx> LateLintPass<'tcx> for LifetimesBoundNestedRef {
     fn check_fn<'tcx2>(
         &mut self,
         cx: &LateContext<'tcx2>,
-        fn_kind: rustc_hir::intravisit::FnKind<'tcx2>,
-        _fn_decl: &'tcx2 rustc_hir::FnDecl<'tcx2>,
-        _body: &'tcx2 rustc_hir::Body<'tcx2>,
-        _span: rustc_span::Span,
-        local_def_id: rustc_span::def_id::LocalDefId,
+        fn_kind: FnKind<'tcx2>,
+        _fn_decl: &'tcx2 FnDecl<'tcx2>,
+        _body: &'tcx2 Body<'tcx2>,
+        _span: Span,
+        local_def_id: LocalDefId,
     ) {
         let FnKind::ItemFn(_ident, generics, _fn_header) = fn_kind else {
             return;
         };
-        if !at_least_2_explicit_lifetimes(generics.params) {
+        let declared_lifetimes = get_declared_lifetimes(generics);
+        if declared_lifetimes.len() <= 1 {
             return;
         }
-        let declared_bounds = get_declared_bounds(generics);
-
-        // collect bounds implied by nested references with lifetimes in input types and output type
-        let mut implied_bounds = BTreeSet::<BoundLftPair>::new();
+        let mut linter = ImpliedBoundsLinter::new(declared_lifetimes, generics);
+        // collect bounds implied by nested references in input types and output type
         let fn_sig = cx.tcx.fn_sig(local_def_id).skip_binder().skip_binder();
         for input_ty in fn_sig.inputs() {
-            collect_nested_ref_implied_bounds(*input_ty, None, &mut implied_bounds);
+            linter.collect_implied_lifetimes_bounds(*input_ty);
         }
-        collect_nested_ref_implied_bounds(fn_sig.output(), None, &mut implied_bounds);
-        report_lints(cx, generics.span, &declared_bounds, &implied_bounds);
+        linter.collect_implied_lifetimes_bounds(fn_sig.output());
+        linter.report_lints(cx);
     }
 
-    /// For issue 84591
-    /// Possibly issue 100051
+    /// For issues 84591 and 100051
     fn check_item_post<'tcx2>(&mut self, cx: &LateContext<'tcx2>, item: &'tcx2 Item<'tcx2>) {
         let ItemKind::Impl(impl_item) = item.kind else {
             return;
@@ -149,25 +148,25 @@ impl<'tcx> LateLintPass<'tcx> for LifetimesBoundNestedRef {
         let Some(of_trait_ref) = impl_item.of_trait else {
             return;
         };
-        if !at_least_2_explicit_lifetimes(impl_item.generics.params) {
+        let declared_lifetimes = get_declared_lifetimes(impl_item.generics);
+        if declared_lifetimes.len() <= 1 {
             return;
         }
-        let declared_bounds = get_declared_bounds(impl_item.generics);
-        let mut implied_bounds = BTreeSet::new();
+        let mut linter = ImpliedBoundsLinter::new(declared_lifetimes, impl_item.generics);
         for path_segment in of_trait_ref.path.segments {
-            let Some(generic_args) = path_segment.args else {
-                continue;
-            };
-            for generic_arg in generic_args.args {
-                if let GenericArg::Type(hir_ty) = generic_arg {
-                    let ty = hir_ty_to_ty(cx.tcx, hir_ty);
-                    collect_nested_ref_implied_bounds(ty, None, &mut implied_bounds);
+            if let Some(generic_args) = path_segment.args {
+                for generic_arg in generic_args.args {
+                    if let GenericArg::Type(hir_arg_ty) = generic_arg {
+                        let arg_ty = hir_ty_to_ty(cx.tcx, hir_arg_ty);
+                        linter.collect_implied_lifetimes_bounds(arg_ty);
+                    }
                 }
             }
         }
-        let for_clause_ty = hir_ty_to_ty(cx.tcx, impl_item.self_ty); // impl ... for   for_clause_type   {}
-        collect_nested_ref_implied_bounds(for_clause_ty, None, &mut implied_bounds);
-        report_lints(cx, impl_item.generics.span, &declared_bounds, &implied_bounds);
+        // issue 10051 for clause: impl ... for for_clause_ty
+        let for_clause_ty = hir_ty_to_ty(cx.tcx, impl_item.self_ty);
+        linter.collect_implied_lifetimes_bounds(for_clause_ty);
+        linter.report_lints(cx);
     }
 }
 
@@ -214,124 +213,153 @@ impl Ord for BoundLftPair {
     }
 }
 
-fn at_least_2_explicit_lifetimes<'a>(generic_params: &'a [GenericParam<'a>]) -> bool {
-    generic_params
+fn get_declared_lifetimes(generics: &Generics<'_>) -> FxHashSet<Symbol> {
+    generics
+        .params
         .iter()
-        .filter(|gp| {
-            matches!(
-                gp.kind,
-                GenericParamKind::Lifetime {
-                    kind: LifetimeParamKind::Explicit,
-                }
-            )
+        .filter_map(|gp| {
+            if let ParamName::Plain(ident) = gp.name {
+                Some(ident.name)
+            } else {
+                None
+            }
         })
-        .enumerate()
-        .any(|(i, _)| i >= 1)
+        .collect()
 }
 
 fn get_declared_bounds(generics: &Generics<'_>) -> BTreeSet<BoundLftPair> {
     let mut declared_bounds = BTreeSet::new();
     for where_predicate in generics.predicates {
         match where_predicate {
-            WherePredicate::BoundPredicate(_) | WherePredicate::EqPredicate(_) => {},
             WherePredicate::RegionPredicate(region_predicate) => {
                 let long_lft_sym = region_predicate.lifetime.ident.name;
                 for generic_bound in region_predicate.bounds {
-                    let GenericBound::Outlives(outlived_lft) = *generic_bound else {
-                        continue;
-                    };
-                    let declared_bound = BoundLftPair::new(long_lft_sym, outlived_lft.ident.name);
-                    declared_bounds.insert(declared_bound);
+                    if let GenericBound::Outlives(outlived_lft) = *generic_bound {
+                        declared_bounds.insert(BoundLftPair::new(long_lft_sym, outlived_lft.ident.name));
+                    }
                 }
             },
+            WherePredicate::BoundPredicate(_) | WherePredicate::EqPredicate(_) => {},
         }
     }
     declared_bounds
 }
 
-#[allow(rustc::usage_of_ty_tykind)]
-fn collect_nested_ref_implied_bounds(
-    ty: Ty<'_>,
-    outlived_lft_sym_opt: Option<Symbol>,
-    implied_bounds: &mut BTreeSet<BoundLftPair>,
-) {
-    match *ty.kind() {
-        TyKind::Ref(ref_region, referred_to_ty, _mutability) => {
-            let mut ref_lft_sym_opt: Option<Symbol> = None;
-            match ref_region.kind() {
-                RegionKind::ReBound(_debruijn_index, bound_region) => {
-                    if let BoundRegionKind::BrNamed(_def_id, ref_lft_sym) = bound_region.kind {
-                        ref_lft_sym_opt = Some(ref_lft_sym);
+struct ImpliedBoundsLinter {
+    declared_lifetimes: FxHashSet<Symbol>,
+    generics_span: Span,                     // for span_lint reporting
+    declared_bounds: BTreeSet<BoundLftPair>, // BTreeSet for consistent reporting order
+    implied_bounds: BTreeSet<BoundLftPair>,  // BTreeSet for consistent reporting order
+}
+
+impl ImpliedBoundsLinter {
+    fn new(declared_lifetimes: FxHashSet<Symbol>, generics: &Generics<'_>) -> Self {
+        ImpliedBoundsLinter {
+            declared_lifetimes,
+            declared_bounds: get_declared_bounds(generics),
+            generics_span: generics.span,
+            implied_bounds: BTreeSet::new(),
+        }
+    }
+
+    fn declared_lifetime_sym(&self, region: Region<'_>) -> Option<Symbol> {
+        let lft_sym_opt = region.get_name();
+        if let Some(lft_sym) = lft_sym_opt
+            && self.declared_lifetimes.contains(&lft_sym)
+        {
+            lft_sym_opt
+        } else {
+            None
+        }
+    }
+
+    fn collect_implied_lifetimes_bounds(&mut self, ty: Ty<'_>) {
+        self.collect_nested_ref_bounds(ty, None);
+    }
+
+    #[allow(rustc::usage_of_ty_tykind)]
+    fn collect_nested_ref_bounds(&mut self, outliving_ty: Ty<'_>, outlived_lft_sym_opt: Option<Symbol>) {
+        let mut outliving_tys = vec![outliving_ty];
+        while let Some(ty) = outliving_tys.pop() {
+            match *ty.kind() {
+                TyKind::Ref(reference_region, referred_to_ty, _mutability) => {
+                    if let Some(declared_lft_sym) = self.declared_lifetime_sym(reference_region) {
+                        if let Some(outlived_lft_sym) = outlived_lft_sym_opt {
+                            self.add_implied_bound(declared_lft_sym, outlived_lft_sym);
+                        }
+                        // ref_lft_sym should be outlived by referred_to_ty
+                        self.collect_nested_ref_bounds(referred_to_ty, Some(declared_lft_sym));
+                    } else {
+                        outliving_tys.push(referred_to_ty);
                     }
                 },
-                RegionKind::ReEarlyParam(early_param_region) => {
-                    if early_param_region.has_name() {
-                        ref_lft_sym_opt = Some(early_param_region.name);
+                TyKind::Tuple(tuple_part_tys) => {
+                    for tuple_part_ty in tuple_part_tys {
+                        outliving_tys.push(tuple_part_ty);
+                    }
+                },
+                TyKind::Array(element_ty, _length) => {
+                    outliving_tys.push(element_ty);
+                },
+                TyKind::Slice(element_ty) => {
+                    outliving_tys.push(element_ty);
+                },
+                TyKind::Alias(AliasKind::Projection, alias_ty) => {
+                    // For issue 10051: the for clause in: impl ... for ... {}
+                    for alias_generic_arg in alias_ty.args {
+                        if let Some(alias_ty) = alias_generic_arg.as_type() {
+                            outliving_tys.push(alias_ty);
+                        };
+                    }
+                },
+                TyKind::Adt(_adt_def, generic_args) => {
+                    // struct/union/enum
+                    if let Some(outlived_lft_sym) = outlived_lft_sym_opt {
+                        for generic_arg in generic_args {
+                            if let Some(arg_region) = generic_arg.as_region()
+                                && let Some(arg_lft_sym) = self.declared_lifetime_sym(arg_region)
+                            {
+                                self.add_implied_bound(arg_lft_sym, outlived_lft_sym);
+                            }
+                        }
                     }
                 },
                 _ => {},
             }
-            if let Some(ref_lft_sym) = ref_lft_sym_opt {
-                if let Some(outlived_lft_sym) = outlived_lft_sym_opt {
-                    // ref_lft_sym outlives outlived_lft_sym
-                    let bound_lft_pair = BoundLftPair::new(ref_lft_sym, outlived_lft_sym);
-                    implied_bounds.insert(bound_lft_pair);
-                }
-                // ref_lft_sym may be outlived by deeper refs
-                collect_nested_ref_implied_bounds(referred_to_ty, Some(ref_lft_sym), implied_bounds);
-            }
-        },
-        TyKind::Tuple(tuple_part_tys) => {
-            for tuple_part_ty in tuple_part_tys {
-                collect_nested_ref_implied_bounds(tuple_part_ty, outlived_lft_sym_opt, implied_bounds);
-            }
-        },
-        TyKind::Array(element_ty, _length) => {
-            collect_nested_ref_implied_bounds(element_ty, outlived_lft_sym_opt, implied_bounds);
-        },
-        TyKind::Slice(element_ty) => {
-            collect_nested_ref_implied_bounds(element_ty, outlived_lft_sym_opt, implied_bounds);
-        },
-        TyKind::Alias(AliasKind::Projection, alias_ty) => {
-            // a for clause in: impl ... for ... {}
-            for alias_generic_arg in alias_ty.args {
-                let Some(alias_ty) = alias_generic_arg.as_type() else {
-                    continue;
-                };
-                collect_nested_ref_implied_bounds(alias_ty, outlived_lft_sym_opt, implied_bounds);
-            }
-        },
-        _ => {},
-    }
-}
-
-fn report_lints(
-    cx: &LateContext<'_>,
-    span: rustc_span::Span,
-    declared_bounds: &BTreeSet<BoundLftPair>,
-    implied_bounds: &BTreeSet<BoundLftPair>,
-) {
-    for implied_bound in implied_bounds.difference(declared_bounds) {
-        span_lint(
-            cx,
-            IMPLICIT_LIFETIMES_BOUND_NESTED_REF,
-            span,
-            &format!(
-                "missing lifetime bound declation: {}",
-                implied_bound.as_bound_declaration()
-            ),
-        );
+        }
     }
 
-    for declared_bound in declared_bounds.intersection(implied_bounds) {
-        span_lint(
-            cx,
-            EXPLICIT_LIFETIMES_BOUND_NESTED_REF,
-            span,
-            &format!(
-                "declared lifetime bound is implied: {}",
-                declared_bound.as_bound_declaration()
-            ),
-        );
+    fn add_implied_bound(&mut self, long_lft_sym: Symbol, outlived_lft_sym: Symbol) {
+        if long_lft_sym != outlived_lft_sym {
+            // only unequal symbols form a lifetime bound
+            self.implied_bounds
+                .insert(BoundLftPair::new(long_lft_sym, outlived_lft_sym));
+        }
+    }
+
+    fn report_lints(self, cx: &LateContext<'_>) {
+        for implied_bound in self.implied_bounds.difference(&self.declared_bounds) {
+            span_lint(
+                cx,
+                IMPLICIT_LIFETIMES_BOUND_NESTED_REF,
+                self.generics_span,
+                &format!(
+                    "missing lifetime bound declaration: {}",
+                    implied_bound.as_bound_declaration()
+                ),
+            );
+        }
+
+        for declared_bound in self.declared_bounds.intersection(&self.implied_bounds) {
+            span_lint(
+                cx,
+                EXPLICIT_LIFETIMES_BOUND_NESTED_REF,
+                self.generics_span,
+                &format!(
+                    "declared lifetime bound is implied: {}",
+                    declared_bound.as_bound_declaration()
+                ),
+            );
+        }
     }
 }
--- tests/ui/lifetimes_bound_nested_ref.rs
+++ tests/ui/lifetimes_bound_nested_ref.rs
@@ -1,6 +1,8 @@
 #![warn(clippy::implicit_lifetimes_bound_nested_ref)]
 #![warn(clippy::explicit_lifetimes_bound_nested_ref)]
 
+use core::mem::MaybeUninit;
+
 // issue 25860, missing bound
 pub fn lifetime_translator_1<'lfta, 'lftb, T>(val_a: &'lfta &'lftb T, _val_b: &'lftb T) -> &'lfta T {
     val_a
@@ -73,6 +75,38 @@ impl<'a: 'b, 'b> Extend2<'a, 'b> for <&'b &'a () as Trait2>::Type2 {
     }
 }
 
-fn main() {
-    // test code goes here
+// from the httparse crate, lib.rs: bounds implied by argument and return types are the same
+// without declared bound:
+unsafe fn deinit_slice_mut<'a, 'b, T>(s: &'a mut &'b mut [T]) -> &'a mut &'b mut [MaybeUninit<T>] {
+    let s: *mut &mut [T] = s;
+    let s = s as *mut &mut [MaybeUninit<T>];
+    &mut *s
 }
+
+// and with declared bound:
+unsafe fn deinit_slice_mut2<'a, 'b: 'a, T>(s: &'a mut &'b mut [T]) -> &'a mut &'b mut [MaybeUninit<T>] {
+    let s: *mut &mut [T] = s;
+    let s = s as *mut &mut [MaybeUninit<T>];
+    &mut *s
+}
+
+// test case for unnamed references.
+// helper declarations:
+struct Thing1<'a> {
+    ref_u8: &'a u8,
+}
+struct Thing2<'a> {
+    ref_u16: &'a u16,
+}
+// without declared bound
+fn test_unnamed_ref<'a, 'b>(w1: &'b mut &mut Thing1<'a>, w2: &mut Thing2<'b>) -> &'a u8 {
+    let _ = w2;
+    w1.ref_u8
+}
+// and with declared bound:
+fn test_unnamed_ref2<'a: 'b, 'b>(w1: &'b mut &mut Thing1<'a>, w2: &mut Thing2<'b>) {
+    let _ = w1;
+    let _ = w2;
+}
+
+fn main() {}
